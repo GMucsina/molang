d7746db8d62325017f776482d44dcf95b86170e4
GMucsina <mucsinagabor@gmail.com>
2017-08-30 16:41:32 +0200

    feat: end of day

--- js/main.js
+++ js/main.js
@@ -20,8 +20,43 @@ piVision.controller("menuController", ["$scope", function($scope) {
 }]);
 
 // Table kezelése
-piVision.controller("tableController", ["$scope", function($scope) {
+piVision.controller("tableController", ["$scope", "$http", function($scope, $http) {
 
-    // Headers
-    $scope.tableHeaders = ["Tralala", "Lalala", "Lelele"]
+    // Connection
+    $scope.tableData = [];
+    $scope.tableHeaders = [];
+    $scope.orderSource = 'name';
+    
+    // Http
+    /*
+    $scope.url="https://raw.githubusercontent.com/jokecamp/FootballData/master/Euro%202016/hungary-players.json";
+    $http.get($scope.url).then( function(res) {
+        $scope.tableData = res.data.sheets.Players;
+        for (var i = 0; i < 5; i++) {
+            $scope.tableData = $scope.tableData.concat(angular.copy($scope.tableData));
+        }
+        $scope.tableHeaders = Object.keys($scope.tableData[0]);
+    }, function(err) {
+        console.error(err);
+    }) ;
+    */
+
+    // Functions
+    $scope.trimHeader = function(str, lgt) {
+        lgt = lgt || 5;
+        return str.substr(0, lgt);
+    }
+
+    $scope.getPlayers = function() {
+        $scope.url="https://raw.githubusercontent.com/jokecamp/FootballData/master/Euro%202016/hungary-players.json";
+        $http.get($scope.url).then( function(res) {
+            $scope.tableData = res.data.sheets.Players;
+            for (var i = 0; i < 5; i++) {
+                $scope.tableData = $scope.tableData.concat(angular.copy($scope.tableData));
+            }
+            $scope.tableHeaders = Object.keys($scope.tableData[0]);
+        }, function(err) {
+            console.error(err);
+        }) ;
+    };
 }]);
