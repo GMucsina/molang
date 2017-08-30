c8c25d1a44639581ba9b78b187297e1c514963e9
GMucsina <mucsinagabor@gmail.com>
2017-08-30 14:34:55 +0200

    feat: Angular cycle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 // Angular Modul létrehozása
 
 var piVision = angular.module("piVision", []);
+
+// Menü kezelése
+piVision.controller("menuController", ["$scope", function($scope) {
+    $scope.brand = "MOL PI System";
+
+    // Menüpontok
+    $scope.menuItems = [
+        {
+            "name": "Home",
+            "href": "#"
+        },
+        {
+            "name": "Contact",
+            "href": "contact"
+        }
+    ]
+}]);
